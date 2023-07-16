1fe9b3217937ceb4fe8638874b9b2eaa868d92ad
rent-n-ride <139553020+rent-n-ride@users.noreply.github.com>
2023-07-16 21:25:19 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,6 @@ const PORT = process.env.PORT || 4000;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 
 let ADMINS = [];
 let USERS = [];
